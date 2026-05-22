80ec082047a24537d69ed2d9d884b0f261d80382
mxhd-kxif-05 <shaikhmdkaif05@gmail.com>
2026-05-22 15:05:25 +0530

    first testing fetch

--- src/app/page.js
+++ src/app/page.js
@@ -1,82 +1,88 @@
+'use client'
 // import Image from "next/image";
+import Navbar from "@/components/Navbar";
+// import { Badge } from "@/components/ui/badge"
+// import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { useState } from "react";
+
+
+export async function fetchMovies(query) {
+
+  const url= `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}include_adult=false&language=en-US&page=1`;
+
+
+    
+  console.log(process.env.NEXT_PUBLIC_TMDB_TOKEN); //data not coming from NEXT_PUBLIC_TMDB_TOKEN i think
+  
+  const options = {
+    method:"GET",
+    headers:{
+      accept:"application/json",
+      Authorization:`Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTJjMTAyZjA0NDlkZjdkYzE0Nzg0N2EzZjFkZWE0NiIsIm5iZiI6MTc1NTY0MDM5OC42NDksInN1YiI6IjY4YTRmMjRlZjMzNzI4MDQwMTNlODJmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-yS-AME0jEvmY65XUYrkf7vJvtNLUV-VqCLrnGshcW8`
+    }
+  };
+
+const res = await fetch(url, options);
+console.log("status:", res.status);
+const data = await res.json();
+console.log("data:", data);
+
+  // const data = await fetchMovies("batman");
+    // console.log(data);
+
+  return data.results;
+
+  
+}
+const [query, setquery] = useState("")
+
 
 export default function Home() {
   return (
-
-      <>
-      <header>
-       <div className="flex justify-between items-center h-16 bg-[#333333] text-white ">navbar
-       </div>
-      </header>
-      <main>
-        <section>
-          <div className="text-white flex flex-col justify-center items-center h-screen">
+        <>
+        <Navbar/>
+      <main >
+        <section className="bg-[#0F172A] h-screen flex justify-center gap-16 items-center flex-col">
+          <div className="text-white flex flex-col justify-center items-center ">
             <label>search bar</label>
-            <input type="text" placeholder="Enter movie name" className="rounded border border-gray-700"/>
+            <input type="text" placeholder="Enter movie name" className="rounded border border-gray-700" onChange={(e) => setQuery(e.target.value)}/>
+             <button onClick={() => fetchMovies(query)}>  ✅
+              button
+            </button>
+          </div>
+          <div className="h-80 w-96 bg-[#fefffe25]">
+             <Card className="relative mx-auto w-full max-w-sm pt-0">
+               <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
+                 <img
+                      src="https://avatar.vercel.sh/shadcn1"
+                     alt="Event cover"
+                     className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+                   />
+               <CardHeader>
+            
+                <CardTitle>Design systems meetup</CardTitle>
+                 <CardDescription>
+                    A practical talk on component APIs, accessibility, and shipping
+                    faster.
+                  </CardDescription>
+                </CardHeader>
+           
+               </Card>
+ 
+
           </div>
         </section>
       </main>
+        
 
-
-      </>
-    // <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    //   <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-    //     <Image
-    //       className="dark:invert"
-    //       src="/next.svg"
-    //       alt="Next.js logo"
-    //       width={100}
-    //       height={20}
-    //       priority
-    //     />
-    //     <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-    //       <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-    //         To get started, edit the page.js file.
-    //       </h1>
-    //       <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-    //         Looking for a starting point or more instructions? Head over to{" "}
-    //         <a
-    //           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //           className="font-medium text-zinc-950 dark:text-zinc-50"
-    //         >
-    //           Templates
-    //         </a>{" "}
-    //         or the{" "}
-    //         <a
-    //           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //           className="font-medium text-zinc-950 dark:text-zinc-50"
-    //         >
-    //           Learning
-    //         </a>{" "}
-    //         center.
-    //       </p>
-    //     </div>
-    //     <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-    //       <a
-    //         className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-    //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         <Image
-    //           className="dark:invert"
-    //           src="/vercel.svg"
-    //           alt="Vercel logomark"
-    //           width='16'
-    //           height={16}
-    //         />
-    //         Deploy Now
-    //       </a>
-    //       <a
-    //         className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-    //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         Documentation
-    //       </a>
-    //     </div>
-    //   </main>
-    // </div>
+        </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,22 @@
-// import React from 'react'
+import React from 'react'
 
-// const Navbar = () => {
-//   return (
-//     <div>Navbar</div>
-//   )
-// }
+const Navbar = () => {
+  return (
+         <>
+      <header>
+       <div className="flex justify-between items-center h-16 bg-[#0B0F19] text-[#F8FAFC] ">navbar
+       </div>
+      </header>
+      {/* <main className="bg-[#0F172A]">
+        <section>
+          <div className="text-white flex flex-col justify-center items-center h-screen">
+            <label>search bar</label>
+            <input type="text" placeholder="Enter movie name" className="rounded border border-gray-700"/>
+          </div>
+        </section>
+      </main> */}
+    </>
+  )
+}
 
-// export default Navbar
+export default Navbar
